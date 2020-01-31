3057cf5d6371215a7481ab3869dfb93718dda6a0
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-30 20:01:37 -0700

    Last commit for today: Added basic html structure in the return.

--- weight-lifting/src/Components/PersonalRecord.jsx
+++ weight-lifting/src/Components/PersonalRecord.jsx
@@ -28,6 +28,27 @@ export default function PersonalRecord() {
       }, []);
 
     return(
+        <div className="pr">
+            <div className="pr-title container">
+                <p>Personal Record</p>
+            </div>
+
+            <div className="pr-numbers container">
+
+                <div className="pr-max-weight container">
+                    <h1 className="pr-max-weight-number">{/* PR Max Weight Number */}</h1>
+                </div>
+
+                <div className="pr-max-reps container">
+                    <h1 className="pr-max-reps-number">{/* PR Max reps Number */}</h1>
+                </div>
+
+            </div>
+
+            <div className="pr-exercise-body-region container">
+                <p className="pr-exercise-body-region">{/* PR Exercise */} | {/* PR Body Region */}</p>
+            </div>
+        </div>
 
     );
 };
