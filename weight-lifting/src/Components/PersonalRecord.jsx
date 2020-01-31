0dbd5f2473060c12631134f8dcc6a4d370712ac5
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-30 19:04:18 -0700

    Setup maxWeight/setMaxWeight useState. Added a skeleton for the useEffect/axios call

--- weight-lifting/src/Components/PersonalRecord.jsx
+++ weight-lifting/src/Components/PersonalRecord.jsx
@@ -2,5 +2,30 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function PersonalRecord() {
+    const [maxWeight, setMaxWeight] = useState([])
 
+    useEffect(() => {
+
+        const getMaxWeight = () => {
+          axios
+          .get("")
+          .then(
+            res => {
+              setMaxWeight(res./*fill this out later*/)
+              console.log("Succesfully recieved data", res)
+            }
+          )
+          .catch(
+            error => {
+              console.error('Server Error', error);
+            }
+          )
+        };
+        getMaxWeight();
+    
+      }, []);
+
+    return(
+
+    );
 };
