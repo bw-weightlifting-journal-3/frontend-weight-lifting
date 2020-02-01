681b8a03f8900efd32b2ab4e9bec8b214901121d
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-01 17:32:27 -0500

    reactstrap menu

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navigation';
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello from App :D</h1>
+      <Navbar />
     </div>
   );
 }
