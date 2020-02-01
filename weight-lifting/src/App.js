524c6af3cbe654ee4fce05cde8f4ef3c2dbd7b20
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-01-31 20:51:52 -0500

    updated

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
-import WorkoutView from './Components/WorkoutView';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Hello from App</h1>
       <WorkoutView />
     </div>
