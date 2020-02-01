5cd691c454f6fc5142d1454f69e84ca4b02b464d
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-01-31 20:24:20 -0500

    Got sticky footer going

--- weight-lifting/src/Components/WorkoutView.js
+++ weight-lifting/src/Components/WorkoutView.js
@@ -8,9 +8,14 @@ const WorkoutView = () => {
   return (
     <Wrapper>
       {/* Placeholder for Navbar */}
+      <h3>This is our Navbar</h3>
       {/* Bring in Personal Record Component */}
+      <h3>This is our Personal Record Component</h3>
       <Header>Worked out Today</Header>
       {/* Map over workouts created */}
+      <h1>Placeholder Card</h1>
+      <h1>Placeholder Card</h1>
+      <h1>Placeholder Card</h1>
 
       {/* Link to the next page */}
       <Button>Start Exercise → </Button>
@@ -22,17 +27,24 @@ export default WorkoutView;
 
 // All of our styled components
 const Button = styled.button`
-  display: flex;
-  margin-top: 90%;
-  background: black;
+  position: fixed;
+  opacity: 0.8;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
   color: yellow;
+  text-align: center;
+  padding: 1.2rem;
+  font-size: 1.5rem;
 `;
 
 const Wrapper = styled.div`
-  background: black;
+  background: darkgrey;
   height: 100vh;
   width: 100vw;
   margin: 0 auto;
+  color: white;
 `;
 
 const Header = styled.h3`
