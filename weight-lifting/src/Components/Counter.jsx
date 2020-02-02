278c611badea8e501b5655f28f1934ef7ba4c3f4
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-01 22:59:14 -0500

    added counter

---
+++ weight-lifting/src/Components/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import CounterButton from "./CounterButton";
+
+export default function Counter1() {
+  const [count1, setCount] = useState(0);
+  const [count2, setCount] = useState(100);
+
+  const addNumber1 = () => {
+    setCount(count => count1 + 1);
+  };
+  const subtractNumber1 = () => {
+    setCount(count => count1 - 1);
+  };
+
+  const addNumber2 = () => {
+    setCount(count => count2 + 1);
+  };
+  const subtractNumber2 = () => {
+    setCount(count => count2 - 1);
+  };
+
+  return (
+    <div className="counter">
+      <div className="top-counter">
+        <CounterButton number={subtractNumber1} buttonValue="-" />
+        <span onClick="">{count1}</span>
+        <CounterButton number={addNumber1} buttonValue="+" />
+      </div>
+      <div className="bottom-counter">
+        <CounterButton number={subtractNumber2} buttonValue="-" />
+        <input type="text">{count2}</span>
+        <CounterButton number={addNumber2} buttonValue="+" />
+      </div>
+    </div>
+  );
+}
