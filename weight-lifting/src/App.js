b1799d3967569c2d6e23bff2a46cd66b9201305b
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-02 18:25:13 -0500

    cleaned files

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-// import Login from "./login";
+import Login from "./login";
 // import ExerciseList from "./Components/ExerciseList";
-import Counter from "./Components/Counter";
+
 function App() {
   return (
     <div className="App">
-      <Counter />
       <h1>Hello from App</h1>
-      {/* <Router>
+      <Router>
         <Route exact path="/login" component={Login} />
       </Router>
-      <WorkoutView /> */}
+      <WorkoutView />
 
     </div>
   );
