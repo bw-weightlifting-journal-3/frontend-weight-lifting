aae62008d32894a417bff5682f544001b34a82ce
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-01 22:07:14 -0500

    added axios call

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./login";
+// import Navigation from "./Components/Navigation";
 // import ExerciseList from "./Components/ExerciseList";
 function App() {
   return (
     <div className="App">
+      {/* <Navigation /> */}
       <h1>Hello from App</h1>
       <Router>
         <Route exact path="/login" component={Login} />
