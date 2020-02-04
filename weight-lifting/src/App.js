b5295dd66adbc93b78da5dcd8583e5ab7e9853dd
Dennis Padiernos <depadiernos@gmail.com>
2020-02-03 19:20:16 -0600

    Update App.js

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./login";
-import WorkoutView from "./Components/WorkoutView";
 // import ExerciseList from "./Components/ExerciseList";
 
 // update
