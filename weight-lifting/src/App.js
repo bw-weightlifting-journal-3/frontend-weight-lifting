57307e68f59a0d6aaa6c1649f25f7d9fb32314fb
Matthew Vaccaro <matth>
2020-02-03 21:18:42 -0600

    working through it

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./login";
-// import ExerciseList from "./Components/ExerciseList";
+// import WorkoutView from "./Components/WorkoutView";
+import ExerciseList from "./Components/ExerciseList";
+import SetList from "./Components/SetList";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello from App</h1>
       <Router>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={ExerciseList} />
+        <Route path="/exercies/:id" component={SetList} />
       </Router>
-      <WorkoutView />
-
+      {/* <WorkoutView /> */}
     </div>
   );
 }
--- weight-lifting/src/Components/ExerciseCard.jsx
+++ weight-lifting/src/Components/ExerciseCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Arrow from "../assets/arrow.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const ExerciseCard = props => {
+  // console.log(props.sets);
   return (
-    <div>
+    <Link to={`/exercies/${props.data.id}`}>
       <div>
-        <h2>{props.data.exercise}</h2>
-        <p>
-          {props.data.sets}Sets | {props.data.focus}
-        </p>
+        <h2>{props.data.name}</h2>
+        <p>{props.sets}</p>
       </div>
       <img src={Arrow} alt="arrow icon" />
-    </div>
+    </Link>
   );
 };
 export default ExerciseCard;
