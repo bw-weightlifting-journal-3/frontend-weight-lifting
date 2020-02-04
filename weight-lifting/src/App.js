727df374f0aca0b6f6d9eecaebb35cd07b270bbb
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-03 19:55:49 -0500

    added navigation to workoutview

--- weight-lifting/src/App.js
+++ weight-lifting/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./login";
+import WorkoutView from "./Components/WorkoutView";
 // import ExerciseList from "./Components/ExerciseList";
 
 function App() {
