49dc883358acf50f98c512d91055febc6b29a079
Matthew Vaccaro <matth>
2020-02-05 18:13:00 -0600

    words

--- weight-lifting/src/Components/SetList.jsx
+++ weight-lifting/src/Components/SetList.jsx
@@ -3,14 +3,15 @@ import AxiosWithAuth from "../utils/AxiosWithAuth";
 import SetCard from "./SetCard";
 
 const SetList = props => {
-  const [sets, setSets] = useState();
+  const [setInfo, setSetInfo] = useState();
+  const [titleUpdate, setTitleUpdate] = useState(false);
 
   useEffect(() => {
     AxiosWithAuth()
       .get(`api/exercises/${props.match.params.id}/sets`)
       .then(res => {
         console.log("res", res);
-        setSets(res.data);
+        setSetInfo(res.data);
       })
       .catch(err => {
         console.log(err);
@@ -19,9 +20,9 @@ const SetList = props => {
 
   return (
     <div>
-      <h2>{props.match.params.id}</h2>
-      {sets
-        ? sets.map(cv => {
+      <h2>{props.location.state.name}</h2>
+      {setInfo
+        ? setInfo.map(cv => {
             return <SetCard data={cv} key={cv.id} />;
           })
         : "You have done nothing"}
